293a05cb4e1b04da887024aa2ae08da0caedffea
kurniacf <kcf170202@gmail.com>
2021-11-23 23:42:15 +0700

    transformation

---
+++ src/com/Component/Player.java
@@ -0,0 +1,49 @@
+package com.Component;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import com.jade.Component;
+import com.util.Constants;
+
+public class Player extends Component {
+	
+	Sprite layerOne, layerTwo, layerThree;
+	public int width, height;
+	
+	public Player(Sprite layerOne, Sprite layerTwo, Sprite layerThree, Color colorOne, Color colorTwo) {
+		
+		this.width = Constants.PLAYER_WIDTH;
+		this.height = Constants.PLAYER_HEIGHT;
+		this.layerOne = layerOne;
+		this.layerTwo = layerTwo;
+		this.layerThree = layerThree;
+		int threshold = 200;
+		
+		for(int y = 0; y < layerOne.image.getWidth(); y++) {
+			for(int x = 0; x < layerOne.image.getHeight(); x++) {
+				Color color = new Color(layerOne.image.getRGB(x, y));
+				if(color.getRed() > threshold || color.getGreen() > threshold || color.getBlue() > threshold) {
+					layerOne.image.setRGB(x, y, colorOne.getRGB());
+				}
+			}
+		}
+		
+		for(int y = 0; y < layerTwo.image.getWidth(); y++) {
+			for(int x = 0; x < layerTwo.image.getHeight(); x++) {
+				Color color = new Color(layerTwo.image.getRGB(x, y));
+				if(color.getRed() > threshold || color.getGreen() > threshold || color.getBlue() > threshold) {
+					layerTwo.image.setRGB(x, y, colorOne.getRGB());
+				}
+			}
+		}
+	}
+	
+	@Override 
+	public void draw(Graphics2D g2) {
+		g2.drawImage(layerOne.image, (int)gameObject.transform.position.x, (int)gameObject.transform.position.y, this.width, this.height, null);
+		g2.drawImage(layerTwo.image, (int)gameObject.transform.position.x, (int)gameObject.transform.position.y, this.width, this.height, null);
+		g2.drawImage(layerThree.image, (int)gameObject.transform.position.x, (int)gameObject.transform.position.y, this.width, this.height, null);
+	}
+	
+}
--- src/com/jade/LevelEditorScene.java
+++ src/com/jade/LevelEditorScene.java
@@ -3,6 +3,7 @@ package com.jade;
 import java.awt.*;
 
 import com.Component.BoxBounds;
+import com.Component.Player;
 import com.Component.Spritesheet;
 import com.dataStructure.AssetPool;
 import com.dataStructure.Transform;
@@ -10,7 +11,7 @@ import com.util.Constants;
 import com.util.Vector2;
 
 public class LevelEditorScene extends Scene{
-	GameObject testObj;
+	GameObject player;
 	public LevelEditorScene(String name) {
 		super.Scene(name);
 	}
@@ -18,14 +19,23 @@ public class LevelEditorScene extends Scene{
 	
 	@Override
 	public void init() {
-		testObj = new GameObject("Some game object", new Transform(new Vector2(100.0f, 300.0f)));
-		Spritesheet spritesheet = new Spritesheet("assets/player/layerOne.png", 42, 42, 2, 13, 13*5);
-		testObj.addComponent(spritesheet.sprites.get(43));
+		player = new GameObject("Some game object", new Transform(new Vector2(100.0f, 300.0f)));
+		Spritesheet layerOne = new Spritesheet("assets/player/layerOne.png", 42, 42, 2, 13, 13*5);
+		Spritesheet layerTwo = new Spritesheet("assets/player/layerTwo.png", 42, 42, 2, 13, 13*5);
+		Spritesheet layerThree = new Spritesheet("assets/player/layerThree.png", 42, 42, 2, 13, 13*5);
+		Player playerComp = new Player(
+				layerOne.sprites.get(0),
+				layerTwo.sprites.get(0),
+				layerThree.sprites.get(0),
+				Color.RED,
+				Color.BLUE
+		);
+		player.addComponent(playerComp);
 	}
 
 	@Override
 	public void update(double up) {
-		testObj.update(up);
+		player.update(up);
 	}
 
 	@Override
@@ -33,7 +43,7 @@ public class LevelEditorScene extends Scene{
 		g2.setColor(Color.BLUE);
 		g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
 	
-		testObj.draw(g2);
+		player.draw(g2);
 	}
 	
 }
